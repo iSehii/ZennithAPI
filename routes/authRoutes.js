25cb4dbf17b72ebd79db28233d5438a667fa2746
iSehii <gsch.sehi@gmail.com>
2024-02-19 01:50:32 -0600

    Nueva conexión y controladores 2

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { login, registro } = require('../controllers/authControllers');
 
-router.get('/login', login);
-router.get('/registro', registro);
+router.post('/login', login);
+router.post('/registro', registro);
 
 module.exports = router;
