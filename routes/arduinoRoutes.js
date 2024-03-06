ba4c711744bb2e0f2d4467bf9e5884a07a318fc3
iSehii <gsch.sehi@gmail.com>
2024-03-05 20:54:29 -0600

    Se agrego el controlador para el arduino 3

--- routes/arduinoRoutes.js
+++ routes/arduinoRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { arduino } = require('../controllers/arduinoControllers');
 
 router.use(express.json());
-router.post('/arduino', arduino);
+router.post('/', arduino);
 
 module.exports = router;
