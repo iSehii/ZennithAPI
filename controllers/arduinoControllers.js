e380bd445e7e3f16a1a33587852fb30a58bd6222
iSehii <gsch.sehi@gmail.com>
2024-03-05 20:56:56 -0600

    Se agrego el controlador para el arduino 5

--- controllers/arduinoControllers.js
+++ controllers/arduinoControllers.js
@@ -7,13 +7,13 @@ router.use(express.json());
 
 exports.arduino = async (req, res) => {
     try {
-        const { message } = req.body;
-        console.log('Se entro en la URL por POST, dato recibido: " '+message+' "');
-        const newArduino = new arduino({ message });
+        const { prueba } = req.body;
+        console.log('Se entro en la URL por POST, dato recibido: " '+prueba+' "');
+        const newArduino = new arduino({ prueba });
         await newArduino.save();
 
-        return res.status(200).json({ message: "Inicio exitoso" });
+        return res.status(200).json({ prueba: "Inicio exitoso" });
     } catch (error) {
-        return res.status(500).json({ message: error.message });
+        return res.status(500).json({ prueba: error.prueba });
     }
 };
